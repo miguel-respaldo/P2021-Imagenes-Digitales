e4de76276febdb34c37836b8ce5e0f242f4b99a9
RobertoMedrano <za2001903@zapopan.tecmm.edu.mx>
2021-03-06 11:20:18 -0600

    Archivo de prueba

--- practica1/abrir-imagen.py
+++ practica1/abrir-imagen.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-img = cv.imread("../imagenes/lenna.jpg")
+img = cv.imread("../imagenes/lenna.jpg", cv.IMREAD_GRAYSCALE)
 
 cv.imshow("Lenna", img)
 
@@ -8,5 +8,4 @@ while True:
     k = cv.waitKey(0)
     if k == 27: # 27 es el escape (ESC)
         break
-
 cv.destroyAllWindows()
