615ce1171ead9e0c7f432f4241bb079b0b4310fb
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2021-03-13 12:29:40 -0600

    coner fixed with color

--- practica2/esquinas_camara.py
+++ practica2/esquinas_camara.py
@@ -17,15 +17,16 @@ while True:
         break
     gray = cv.cvtColor(imagen,cv.COLOR_BGR2GRAY)
     corners= cv.goodFeaturesToTrack(gray,25,0.01,10)
+    color = cv.cvtColor(gray,cv.COLOR_GRAY2RGB)
 
     if corners is not None:
         corners = np.int0(corners)
 
         for i in corners:
             x,y = i.ravel()
-            cv.circle(gray,(x,y),3,(0,0,255),-1)
+            cv.circle(color,(x,y),3,(0,0,255),-1)
 
-    cv.imshow("Camara", gray)
+    cv.imshow("Camara", color)
 
     if cv.waitKey(1) == 27:
         break
