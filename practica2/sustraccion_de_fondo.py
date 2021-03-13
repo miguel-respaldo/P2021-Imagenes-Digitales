82c46fdac2682d52cc8deab480c105c52d0f02cc
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2021-03-13 11:31:17 -0600

    removiendo el fondo

---
+++ practica2/sustraccion_de_fondo.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+
+backSub = cv.createBackgroundSubtractorMOG2()
+# backSub = cv.createBackgroundSubtractorKNN()
+
+capture = cv.VideoCapture(0)
+
+if not capture.isOpened():
+    print("No puedo abrir la camara")
+    exit(1)
+
+while True:
+    ret, frame = capture.read()
+
+    if frame is None:
+        break
+
+    fgMask = backSub.apply(frame)
+
+    cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
+    cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+    cv.imshow('Frame', frame)
+    cv.imshow('FG Mask', fgMask)
+
+    keyboard = cv.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
