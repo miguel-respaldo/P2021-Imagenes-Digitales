22671bbf3330ea96865919cccfc4c63188ef4f76
kepler <kepler@yalochat.com>
2021-03-13 11:15:17 -0600

    filtro gaussiano

---
+++ practica2/filtro_gaussiano.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+
+def main():
+    window_name = ('Sobel Demo - Simple Edge Detector')
+    scale = 1
+    delta = 0
+    ddepth = cv.CV_16S
+
+    camara = cv.VideoCapture(0)
+
+    if not camara.isOpened():
+        print("No puedo abrir la camara")
+        exit(1)
+
+    while True:
+        # Leemos la imagen de la camara
+        ret, imagen = camara.read()
+        # ret --> confirmacion de leer la camara
+        # imagen --> los datos de imagen
+
+        if not ret:
+            print("No podemos capturar la imagen de la camara")
+            break
+
+        imagen = cv.GaussianBlur(imagen, (3, 3), 0)
+
+        cv.imshow(window_name, imagen)
+
+        if cv.waitKey(1) == 27:
+            break
+
+    camara.release()
+    cv.destroyAllWindows()
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
