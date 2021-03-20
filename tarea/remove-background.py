53690cd3caf3ccf3ba197df9206a281ee7a1bcb9
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2021-03-20 09:39:45 -0600

    restructure program

--- tarea/remove-background.py
+++ tarea/remove-background.py
@@ -27,8 +27,6 @@ ret, frame = capture.read()
 if frame is None:
     exit()
 
-
-
 # Redimencionamos el fondo al tamañano de la imagen capturada
 width  = frame.shape[1]
 height = frame.shape[0]
@@ -40,31 +38,38 @@ fondo = cv.resize(fondo,dsize)
 while ret:
     ret, frame = capture.read()
 
+    if ret != True:        # Convert image to grayscale
+        break
+
     # Volteamos la imagen para que se vea como espejo
     frame = cv.flip(frame, 1)
 
-    if ret == True:        # Convert image to grayscale
-        image_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)        # Apply Canny Edge Dection
-        edges = cv.Canny(image_gray, canny_low, canny_high)
+    image_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)        # Apply Canny Edge Dection
+    edges = cv.Canny(image_gray, canny_low, canny_high)
 
-        edges = cv.dilate(edges, None)
-        edges = cv.erode(edges, None)
+    edges = cv.dilate(edges, None)
+    edges = cv.erode(edges, None)
 
-        contour_info = [(c, cv.contourArea(c),) for c in cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)[1]]
+    contour_info = [(c, cv.contourArea(c),) for c in cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)[1]]
 
-        # Get the area of the image as a comparison
-        image_area = frame.shape[0] * frame.shape[1]
+    # Get the area of the image as a comparison
+    image_area = frame.shape[0] * frame.shape[1]
 
-        # calculate max and min areas in terms of pixels
-        max_area = max_area * image_area
-        min_area = min_area * image_area
+    # calculate max and min areas in terms of pixels
+    max_area = max_area * image_area
+    min_area = min_area * image_area
 
-        # Set up mask with a matrix of 0's
-        mask = np.zeros(edges.shape, dtype = np.uint8)
+    # Set up mask with a matrix of 0's
+    mask = np.zeros(edges.shape, dtype = np.uint8)
 
-        cv.imshow('Original', frame)
-        cv.imshow('Procesada', edges)
+    cv.imshow('Original', frame)
+    cv.imshow('Procesada', edges)
 
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
         break
+
+# Release the VideoCapture object
+# Shut down
+cv.destroyAllWindows()
+capture.release()
