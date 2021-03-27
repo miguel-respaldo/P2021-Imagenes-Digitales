ccb6cb36f05c97f24cc344e9ec676f8cb29be507
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2021-03-27 09:47:19 -0600

    canales de color

---
+++ practica4/03_imagenes_por_canal.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+camara = cv.VideoCapture(0)
+
+if not camara.isOpened():
+    print("No puedo abrir la camara")
+    exit(1)
+
+while True:
+    # Leemos la imagen de la camara
+    ret, imagen = camara.read()
+
+    if not ret:
+        print("No podemos capturar la imagen de la camara")
+        break
+
+    # Separar los canales en RGB
+    b,g,r = cv.split(img)
+
+    cv.imshow("Azul",b)
+    cv.imshow("Verde",g)
+    cv.imshow("Rojo",r)
+
+    if cv.waitKey(1) == 27:
+        break
+
+camara.release()
+cv.destroyAllWindows()
